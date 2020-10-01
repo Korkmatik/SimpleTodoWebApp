19123182421ae02b7e22ffc26f1ac6f6e8354ad0
Korkmatik <9c54efc1-143b-4f5f-9d2f-f6f189583f0f@aleeas.com>
2020-10-01 22:09:30 +0200

    Added functionality to create a todo

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 var searchInput = document.querySelector('#search-input');
+var listUl = document.querySelector('#list-items');
 var listItems = document.querySelectorAll('#list-items li');
+var newTodoInput = document.querySelector('#new-todo');
+var addTodoBtn = document.querySelector('#create-todo-btn');
 
 searchInput.addEventListener('input', search);
+addTodoBtn.addEventListener('click', addTodoAndClearInput);
 
+
+/* Searching for a Todo */
 function search(e) {
     var value = e.target.value.toLowerCase();
 
@@ -16,4 +22,48 @@ function search(e) {
             element.style.display = 'none';
         }
     });
+}
+
+/* Adding a Todo */
+function addTodoAndClearInput(e) {
+    addTodoToList(newTodoInput.value);
+    clearInput();
+}
+
+function addTodoToList(todo) {
+    contentSpan = createContentSpan(todo);
+    deleteBtn = createDeleteBtn();
+
+    listUl.appendChild(
+        createTodoNode(contentSpan, deleteBtn)
+    );
+}
+
+function createContentSpan(todo) {
+    contentSpan = document.createElement('span');
+    contentSpan.classList.add('content');
+    contentSpan.appendChild(document.createTextNode(todo));
+    
+    return contentSpan;
+}
+
+function createDeleteBtn() {
+    deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("delete");
+    deleteBtn.setAttribute('type', 'button');
+    deleteBtn.appendChild(document.createTextNode("X"));
+
+    return deleteBtn;
+}
+
+function createTodoNode(contentSpan, deleteBtn) {
+    todoNode = document.createElement('li');
+    todoNode.appendChild(contentSpan);
+    todoNode.appendChild(deleteBtn)
+
+    return todoNode;
+}
+
+function clearInput() {
+    newTodoInput.value = "";
 }
